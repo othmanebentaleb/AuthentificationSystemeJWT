55a86a6c3e49e41da0d9de1c19dfd52247215570
unknown <othmanebentaleb90@gmail.com>
2018-06-13 19:35:55 +0200

    Projet initial

---
+++ JWTSystemAuthentication/src/main/java/org/system/authentication/jwt/JWTSystemAuthentication/JwtSystemAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package org.system.authentication.jwt.JWTSystemAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSystemAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSystemAuthenticationApplication.class, args);
+	}
+}
