25388f460e38e88b04e7e64e705c20d6f82d8572
unknown <othmanebentaleb90@gmail.com>
2018-06-23 16:03:13 +0200

    Désactivation d'authentification avec des sessions

--- JWTSystemAuthentication/src/main/java/org/system/authentication/jwt/JWTSystemAuthentication/security/SecurityConfig.java
+++ JWTSystemAuthentication/src/main/java/org/system/authentication/jwt/JWTSystemAuthentication/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -31,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
            http.formLogin();
            http.csrf().disable();
+           http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
            http.authorizeRequests().anyRequest().authenticated();
     }
 }
